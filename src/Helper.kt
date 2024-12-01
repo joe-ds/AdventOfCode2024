1935265bf3db2ab5543adc4681382542b0a9590d
Joe <73705768+joe-ds@users.noreply.github.com>
2024-12-01 22:08:05 +0000

    Day One!

--- src/Helper.kt
+++ src/Helper.kt
@@ -1,2 +1,15 @@
-class Helper {
+import java.io.File
+
+abstract class Day(val dNo: Int) {
+    open fun partOne(): String = "Part One not implemented yet."
+    open fun partTwo(): String = "Part Two not implemented yet."
+}
+
+fun Day.getInputAsLines(): List<String> {
+    val filename: String = "inputs/" + (this.dNo.toString()).padStart(2, '0')
+    return File(filename).readLines().map{ it.trim() }
+}
+
+fun Day.print() {
+    println("Day ${this.dNo}:\n\tPart One: ${this.partOne()}\n\tPart Two: ${this.partTwo()}\n\n")
 }
